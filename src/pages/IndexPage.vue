91283cab3f49a3444ba9a9771b88c55fce758fb1
alexalaska90 <chrisalexonn@gmail.com>
2023-10-30 23:24:55 +0300

    filled card added

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,7 +4,9 @@
       <div class="row justify-between items-center">
         <p class="main-heading">Карточки</p>
         <div class="row q-gutter-x-sm items-center">
-          <div class="text-primary caption">Проект:</div>
+          <div class="text-primary caption">
+            <span>Проект:</span>
+          </div>
           <q-btn-dropdown
             class="custom-dropdown-toggle"
             text-color="info"
@@ -31,8 +33,62 @@
         </div>
       </div>
     </section>
+    <section>
+      <q-card flat class="bg-warning card">
+        <q-card-section>
+          <div class="row justify-between items-center q-mb-md">
+            <div class="card-heading">
+              <span class="text-primary q-mr-xs">&bull;</span>
+              <span>Стадия 1</span>
+            </div>
+            <div class="row q-gutter-x-xs">
+              <img
+                class="cursor-pointer"
+                src ="../assets/arrow_down_gray.svg"
+                alt="arrow down"
+              />
+              <img
+                class="cursor-pointer"
+                src ="../assets/arrow_up_gray.svg"
+                alt="arrow up"
+              />
+            </div>
+          </div>
+          <div class="bg-white card-section column justify-between">
+            <div class="q-qutter-y-xs">
+              <div class="row justify-between">
+                <div class="row q-gutter-x-sm">
+                  <div class="card-heading">
+                    <span>Карточка 1</span>
+                  </div>
+                  <img
+                    class="cursor-pointer"
+                    src ="../assets/edit.svg"
+                    alt="edit"
+                  />
+                  <img
+                    class="cursor-pointer"
+                    src ="../assets/delete.svg"
+                    alt="delete"
+                  />
+                </div>
+                <img
+                    class="cursor-pointer"
+                    src ="../assets/drag.svg"
+                    alt="drag"
+                  />
+              </div>
+              <div class="text-primary">
+                <span class="caption">Балл: </span>
+                <span class="text-dark caption bold-caption">3.2</span>
+              </div>
+            </div>
+            <q-card flat bordered class="bg-warning flex flex-center project">
+              <span class="text-info">Проект 1</span>
+            </q-card>
+          </div>
+        </q-card-section>
+      </q-card>
+    </section>
   </q-page>
 </template>
-
-<script setup lang="ts">
-</script>
