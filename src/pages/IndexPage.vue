a4af820f614c6f6f4921a04b1561e930762bfda1
alexalaska90 <chrisalexonn@gmail.com>
2023-10-30 22:04:37 +0300

    custom toggle added to dropdown button

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,24 @@
 <template>
   <q-page class="bg-secondary container">
-    test
+    <section>
+      <div class="row justify-between items-center">
+        <p class="main-heading">Карточки</p>
+        <div class="row">
+          <div>
+            <p class="text-primary caption">Проект:</p>
+          </div>
+          <q-btn-dropdown
+            class="custom-dropdown-toggle"
+            text-color="info"
+            no-caps
+            color="warning"
+            unelevated
+            label="Не выбрано"
+          >
+          </q-btn-dropdown>
+        </div>
+      </div>
+    </section>
   </q-page>
 </template>
 
