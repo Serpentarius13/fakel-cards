4352d867202e4d061707e53136f7e50701e46c86
alexalaska90 <chrisalexonn@gmail.com>
2023-10-31 21:12:10 +0300

    empty card design added

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -97,7 +97,7 @@
           <div class="row justify-between items-center q-mb-md">
             <div class="card-heading">
               <span class="text-primary q-mr-xs">&bull;</span>
-              <span>Стадия 1</span>
+              <span>Стадия 4</span>
             </div>
             <div class="row q-gutter-x-xs">
               <img
@@ -112,38 +112,10 @@
               />
             </div>
           </div>
-          <div class="bg-white card-section column justify-between">
-            <div class="q-qutter-y-xs">
-              <div class="row justify-between">
-                <div class="row q-gutter-x-sm">
-                  <div class="card-heading">
-                    <span>Карточка 1</span>
-                  </div>
-                  <img
-                    class="cursor-pointer"
-                    src ="../assets/edit.svg"
-                    alt="edit"
-                  />
-                  <img
-                    class="cursor-pointer"
-                    src ="../assets/delete.svg"
-                    alt="delete"
-                  />
-                </div>
-                <img
-                    class="cursor-pointer"
-                    src ="../assets/drag.svg"
-                    alt="drag"
-                  />
-              </div>
-              <div class="text-primary">
-                <span class="caption">Балл: </span>
-                <span class="text-dark caption bold-caption">3.2</span>
-              </div>
+          <div class="bg-warning card-section empty-section flex flex-center">
+            <div class="text-primary">
+              <span>Список пуст</span>
             </div>
-            <q-card flat bordered class="bg-warning flex flex-center project">
-              <span class="text-info">Проект 1</span>
-            </q-card>
           </div>
           <div class="row justify-center q-mt-md">
             <q-btn flat no-caps label="Добавить" color="warning" text-color="primary" />
